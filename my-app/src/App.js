bdeb69e28d22518cdb93c13df45e680f4112b143
Ryan Schultz <ryan.schultz1997@gmail.com>
2018-09-15 03:02:32 -0400

    working static chat

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ChatMessage extends Component {
+  render() {
+    const message = this.props.message;
+    const type = (this.props.type === "self") ? "Self-message" : "Friend-message";
+    return(
+        <tr>
+          <td className={type}> {message} </td>
+        </tr>
+    );
+  }
+}
+
+class ChatView extends Component {
+  render() {
+    const rows = [];
+    let lastMessage = null;
+
+    this.props.messages.forEach((message) => {
+      if (message.content !== lastMessage){
+        rows.push(
+            <ChatMessage
+                message={message.content}
+                type={message.type} 
+                key={message.content} />
+          );
+      }
+    });
+
+    return(
+      <table className="Chat-view">
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+const CHAT_LOG = [
+  {content: 'Hey Ben!', type: 'self'},
+  {content: 'Hey Ryan!', type: 'friend'},
+  {content: 'How is it going?', type: 'self'},
+  {content: 'Well thanks, hbu?', type: 'friend'}
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Smooth Connect
+          </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello world
         </p>
+        <ChatView messages={CHAT_LOG} />
       </div>
     );
   }
